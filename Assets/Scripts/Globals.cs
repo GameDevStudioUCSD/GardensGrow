502cc4d2e29d27d59bd7934f35f31a5a9b98eb35
Steven Lee <ssl024@ucsd.edu>
2016-10-22 15:50:14 -0700

    Add TileMap and beginning of Navigation.
    Created new scene Navigation.
    Created TileMapScript and TileScript.
    We have movement checks working.

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -2,4 +2,6 @@
 public class Globals {
 
     public const float pixelSize = 0.125f;
+
+    public enum Direction { North, East, South, West };
 }
--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class TileScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
