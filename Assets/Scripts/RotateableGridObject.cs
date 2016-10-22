b235e501a3ebdf536bea35d900eb75469d734b70
Eric Cheng <ericcheng95@gmail.com>
2016-10-21 21:01:24 -0700

    added hierarchy

---
+++ Assets/Scripts/RotateableGridObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateableGridObject : GridObject {
+
+	// 0 = South, 1 = West, 2 = North, 3 = East
+	private int direction;
+	public BoxCollider2D face;
+	
+	// Use this for initialization
+	void Start () {
+		direction = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Changes direction and direction sprite
+	public void Rotate(int rotate)
+	{
+		direction = rotate;
+		Debug.Log(direction);
+
+	}
+}
---
+++ Assets/Scripts/StaticGridObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticGridObject : GridObject {
+
+	public bool isBarrier;
+	public BoxCollider2D barrier;
+
+	// Use this for initialization
+	void Start () {
+		if (!isBarrier)
+		{
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
