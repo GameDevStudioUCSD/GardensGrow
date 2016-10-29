78ff90c4116d5f4b9903f961428745a754b54057
Steven Lee <ssl024@ucsd.edu>
2016-10-28 20:59:51 -0700

    Update tile map script + navigation
    New folders for tile map and ai
    Working on monster ai.

---
+++ Assets/Scripts/EnemyAI/SimpleMonsterAI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMonsterAI : MonoBehaviour {
+
+    // searching how far from goal (heuristic) and how far from start (path cost)
+
+	// Use this for initialization
+	void Start () {
+        
+
+        // Priority Queue that sorts lowest cost to highest cost
+        // From start position get all successor nodes
+        // Calculate total cost for each node
+        // add these nodes into PQ
+        // While PQ not empty
+        // Pop node
+        // Check if goal
+        // get all successors
+        // calculate
+        // push into PQ
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    int heuristic(Vector2 currentPosition, Vector2 goalPosition) 
+    {
+        return (int)Vector2.Distance(currentPosition, goalPosition);
+    }
+
+    int pathCost(Vector2 currentPosition, Vector2 startPosition) 
+    {
+        // TODO:
+        return 0;
+    }
+}
---
+++ Assets/Scripts/TileScripts/ActionState.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ActionState {
+    public Vector2 position;
+    public Globals.Direction direction;
+
+    public ActionState(Vector2 position, Globals.Direction direction)
+    {
+        this.position = position;
+        this.direction = direction;
+    }
+
+    public override string ToString()
+    {
+        //return base.ToString();
+        string output = "Position: " + position + " Direction: " + direction;
+
+        return output;
+    }
+}
