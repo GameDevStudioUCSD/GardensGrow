576c8805e92580c9f01880d044ffee7d1a8b4320
Michael <mmg005@ucsd.edu>
2016-10-29 16:57:27 -0700

    Added random motion to SimpleMonsterAI

--- Assets/Scripts/EnemyAI/SimpleMonsterAI.cs
+++ Assets/Scripts/EnemyAI/SimpleMonsterAI.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class SimpleMonsterAI : MonoBehaviour {
 
+    public EnemyGridObject entity;
+    public float updateRate = 1;
+    [Range(0,1)]
+    public float chanceToChangeDirection;
+    private Globals.Direction movementDirrection;
     // searching how far from goal (heuristic) and how far from start (path cost)
 
 	// Use this for initialization
 	void Start () {
-        
-
         // Priority Queue that sorts lowest cost to highest cost
         // From start position get all successor nodes
         // Calculate total cost for each node
@@ -19,13 +22,23 @@ public class SimpleMonsterAI : MonoBehaviour {
         // get all successors
         // calculate
         // push into PQ
-	
+        InvokeRepeating("RandomMovement", 0, 1.0f/updateRate);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        Debug.DrawRay(transform.position, Globals.DirectionToVector(entity.direction));
 	}
+
+    void RandomMovement()
+    {
+        if(Random.value < chanceToChangeDirection)
+        {
+            int m = Random.Range(0, 4);
+            movementDirrection = (Globals.Direction)m;
+        }
+        entity.Move(movementDirrection);
+    }
 
     int heuristic(Vector2 currentPosition, Vector2 goalPosition) 
     {
