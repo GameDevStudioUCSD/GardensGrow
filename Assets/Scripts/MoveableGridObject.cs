83411325cb771889aa5f13bd36fc782dc8f2dd76
Daniel <griffithsdanielc@gmail.com>
2016-11-04 21:26:11 -0700

    Player animator partially implemented

--- Assets/Scripts/MoveableGridObject.cs
+++ Assets/Scripts/MoveableGridObject.cs
@@ -14,17 +14,11 @@ public class MoveableGridObject : KillableGridObject {
 	private bool northCollision = false;
 	private bool eastCollision = false;
 
-    public Sprite IdleSpriteSouth;
-    public Sprite IdleSpriteWest;
-    public Sprite IdleSpriteNorth;
-    public Sprite IdleSpriteEast;
-    public int walkSpriteFrameCount = 10;
-    protected int currentFrame = 0;
-    protected int currentSprite = 0;
-    public System.Collections.Generic.List<Sprite> WalkSpriteSouth;
-    public System.Collections.Generic.List<Sprite> WalkSpriteWest;
-    public System.Collections.Generic.List<Sprite> WalkSpriteNorth;
-    public System.Collections.Generic.List<Sprite> WalkSpriteEast;
+    private Animator animator;
+
+    protected virtual void Start() {
+        animator = GetComponent<Animator>();
+    }
 
 	protected virtual void Update() {
 		base.Update();
@@ -33,198 +27,61 @@ public class MoveableGridObject : KillableGridObject {
 	// Direction: 0 = South, 1 = West, 2 = North, 3 = East
 	public virtual void Move(Globals.Direction direction) {
 		Rotate(direction);
-		if (direction == Globals.Direction.South && !southCollider.isTriggered) {
+        if (animator.GetInteger("Direction") != (int)direction)
+            animator.SetInteger("Direction", (int)direction);
+        animator.SetBool("IsWalking", true);
+        if (direction == Globals.Direction.South && !southCollider.isTriggered) {
 			Vector3 position = this.transform.position;
             position.y -= pixelSize;
             this.transform.position = position;
-            if (direction == Globals.Direction.South) {
-                currentFrame++;
-                if (currentFrame >= walkSpriteFrameCount) {
-                    currentFrame = 0;
-                    currentSprite++;
-                    if (currentSprite >= WalkSpriteSouth.Count) currentSprite = 0;
-                    gameObject.GetComponent<SpriteRenderer>().sprite = WalkSpriteSouth[currentSprite];
-                }
-            }
-            else {
-                direction = Globals.Direction.South;
-                currentFrame = 0;
-                currentSprite = 0;
-                gameObject.GetComponent<SpriteRenderer>().sprite = WalkSpriteSouth[0];
-            }
         }
 		else if (direction == Globals.Direction.West && !westCollider.isTriggered) {
 			Vector3 position = this.transform.position;
             position.x -= pixelSize;
             this.transform.position = position;
-            if (direction == Globals.Direction.West) {
-                currentFrame++;
-                if (currentFrame >= walkSpriteFrameCount) {
-                    currentFrame = 0;
-                    currentSprite++;
-                    if (currentSprite >= WalkSpriteWest.Count) currentSprite = 0;
-                    gameObject.GetComponent<SpriteRenderer>().sprite = WalkSpriteWest[currentSprite];
-                }
-            }
-            else {
-                direction = Globals.Direction.West;
-                currentFrame = 0;
-                currentSprite = 0;
-                gameObject.GetComponent<SpriteRenderer>().sprite = WalkSpriteWest[0];
-            }
         }
 		else if (direction == Globals.Direction.North && !northCollider.isTriggered) {
 			Vector3 position = this.transform.position;
             position.y += pixelSize;
             this.transform.position = position;
-            if (direction == Globals.Direction.North) {
-                currentFrame++;
-                if (currentFrame >= walkSpriteFrameCount) {
-                    currentFrame = 0;
-                    currentSprite++;
-                    if (currentSprite >= WalkSpriteNorth.Count) currentSprite = 0;
-                    gameObject.GetComponent<SpriteRenderer>().sprite = WalkSpriteNorth[currentSprite];
-                }
-            }
-            else {
-                direction = Globals.Direction.North;
-                currentFrame = 0;
-                currentSprite = 0;
-                gameObject.GetComponent<SpriteRenderer>().sprite = WalkSpriteNorth[0];
-            }
         }
 		else if (direction == Globals.Direction.East && !eastCollider.isTriggered) {
 			Vector3 position = this.transform.position;
             position.x += pixelSize;
             this.transform.position = position;
-            if (direction == Globals.Direction.East)
-            {
-                currentFrame++;
-                if (currentFrame >= walkSpriteFrameCount)
-                {
-                    currentFrame = 0;
-                    currentSprite++;
-                    if (currentSprite >= WalkSpriteEast.Count) currentSprite = 0;
-                    gameObject.GetComponent<SpriteRenderer>().sprite = WalkSpriteEast[currentSprite];
-                }
-            }
-            else
-            {
-                direction = Globals.Direction.East;
-                currentFrame = 0;
-                currentSprite = 0;
-                gameObject.GetComponent<SpriteRenderer>().sprite = WalkSpriteEast[0];
-            }
         }
 	}
     public virtual void MoveEnemy(Globals.Direction direction)
     {
         Rotate(direction);
+        animator.SetInteger("Direction", (int)direction);
+        animator.SetBool("IsWalking", true);
         if (direction == Globals.Direction.South && !southCollider.isTriggered)
         {
             Vector3 position = this.transform.position;
             position.y -= pixelSize;
             this.transform.position = position;
-            if (direction == Globals.Direction.South)
-            {
-                currentFrame++;
-                if (currentFrame >= walkSpriteFrameCount)
-                {
-                    currentFrame = 0;
-                    currentSprite++;
-                    if (currentSprite >= WalkSpriteSouth.Count) currentSprite = 0;
-                  
-                }
-            }
-            else
-            {
-                direction = Globals.Direction.South;
-                currentFrame = 0;
-                currentSprite = 0;
-                
-            }
         }
         else if (direction == Globals.Direction.West && !westCollider.isTriggered)
         {
             Vector3 position = this.transform.position;
             position.x -= pixelSize;
             this.transform.position = position;
-            if (direction == Globals.Direction.West)
-            {
-                currentFrame++;
-                if (currentFrame >= walkSpriteFrameCount)
-                {
-                    currentFrame = 0;
-                    currentSprite++;
-                    if (currentSprite >= WalkSpriteWest.Count) currentSprite = 0;
-                    
-                }
-            }
-            else
-            {
-                direction = Globals.Direction.West;
-                currentFrame = 0;
-                currentSprite = 0;
-                
-            }
         }
         else if (direction == Globals.Direction.North && !northCollider.isTriggered)
         {
             Vector3 position = this.transform.position;
             position.y += pixelSize;
             this.transform.position = position;
-            if (direction == Globals.Direction.North)
-            {
-                currentFrame++;
-                if (currentFrame >= walkSpriteFrameCount)
-                {
-                    currentFrame = 0;
-                    currentSprite++;
-                    if (currentSprite >= WalkSpriteNorth.Count) currentSprite = 0;
-                    
-                }
-            }
-            else
-            {
-                direction = Globals.Direction.North;
-                currentFrame = 0;
-                currentSprite = 0;
-                
-            }
         }
         else if (direction == Globals.Direction.East && !eastCollider.isTriggered)
         {
             Vector3 position = this.transform.position;
             position.x += pixelSize;
             this.transform.position = position;
-            if (direction == Globals.Direction.East)
-            {
-                currentFrame++;
-                if (currentFrame >= walkSpriteFrameCount)
-                {
-                    currentFrame = 0;
-                    currentSprite++;
-                    if (currentSprite >= WalkSpriteEast.Count) currentSprite = 0;
-                    
-                }
-            }
-            else
-            {
-                direction = Globals.Direction.East;
-                currentFrame = 0;
-                currentSprite = 0;
-                
-            }
         }
     }
     protected virtual void Stop() {
-        if (direction == Globals.Direction.South)
-            gameObject.GetComponent<SpriteRenderer>().sprite = IdleSpriteSouth;
-        else if (direction == Globals.Direction.West)
-            gameObject.GetComponent<SpriteRenderer>().sprite = IdleSpriteWest;
-        else if (direction == Globals.Direction.North)
-            gameObject.GetComponent<SpriteRenderer>().sprite = IdleSpriteNorth;
-        else if (direction == Globals.Direction.East)
-            gameObject.GetComponent<SpriteRenderer>().sprite = IdleSpriteEast;
+        animator.SetBool("IsWalking", false);
     }
 }
