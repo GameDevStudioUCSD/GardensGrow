9a7fd280d4517dcdc4b34027816ea792ef342b41
andrewhongnguyen <andrewhongnguyen@gmail.com>
2016-11-10 14:12:42 -0800

    "trying to pull everything"

--- Assets/Scripts/EnemyGridObject.cs
+++ Assets/Scripts/EnemyGridObject.cs
@@ -2,10 +2,17 @@
 using System.Collections;
 
 public class EnemyGridObject : MoveableGridObject {
+    
 
 	// Use this for initialization
 	protected virtual void Start () {
 
+        Vector3 hpSpawn = new Vector3(GetComponent<Transform>().position.x, this.GetComponent<Transform>().position.y+2, 0.0f);
+        Quaternion rt = Quaternion.identity;
+        Instantiate(hpBar,hpSpawn,rt);
+        
+
+        Debug.Log("hi");
 	}
 
 	// Update is called once per frame
