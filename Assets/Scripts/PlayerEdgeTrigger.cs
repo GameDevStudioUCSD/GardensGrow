8643709164ca5b19119f6b0f1deba81b6f5e3f88
Ajeya Rengarajan <ajrengar@ucsd.edu>
2016-11-12 15:45:57 -0800

    fixed movement after kill

--- Assets/Scripts/PlayerEdgeTrigger.cs
+++ Assets/Scripts/PlayerEdgeTrigger.cs
@@ -1,38 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class PlayerEdgeTrigger : MonoBehaviour {
+public class PlayerEdgeTrigger : MonoBehaviour
+{
 	public bool isTriggered;
-	private List<KillableGridObject> killList = new List<KillableGridObject>();
+	Collider2D other;   	// the other collider
+	private List<KillableGridObject> killList = new List<KillableGridObject> ();
 
-	void OnTriggerEnter2D(Collider2D other) {
-        if (!other.isTrigger)
-		isTriggered = true;
-
-		KillableGridObject killable = other.GetComponent<KillableGridObject>();
-        if (killable != null)
-        {
-        	killList.Add(killable);
-    	}
+	void Update ()
+	{
+		if (isTriggered && !other) {
+			isTriggered = false;
+		}
 	}
 
-	void OnTriggerStay2D(Collider2D other) { 
-        if (!other.isTrigger)
-		isTriggered = true;
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (!other.isTrigger)
+			isTriggered = true;
+
+		KillableGridObject killable = other.GetComponent<KillableGridObject> ();
+		if (killable != null) {
+			killList.Add (killable);
+		}
+
+		this.other = other;
 	}
 
-	void OnTriggerExit2D(Collider2D other) {
-        if(!other.isTrigger)
-		isTriggered = false;
+	void OnTriggerStay2D (Collider2D other)
+	{ 
+		if (!other.isTrigger)
+			isTriggered = true;
 
-		KillableGridObject killable = other.GetComponent<KillableGridObject>();
-        if (killable != null)
-        {
-       		killList.Remove(killable);
-    	}
+		this.other = other;
 	}
 
-	public List<KillableGridObject> getList()
+	void OnTriggerExit2D (Collider2D other)
+	{
+		if (!other.isTrigger)
+			isTriggered = false;
+
+		KillableGridObject killable = other.GetComponent<KillableGridObject> ();
+		if (killable != null) {
+			killList.Remove (killable);
+		}
+
+		this.other = other;
+
+	}
+
+	public List<KillableGridObject> getList ()
 	{
 		return killList;
 	}
