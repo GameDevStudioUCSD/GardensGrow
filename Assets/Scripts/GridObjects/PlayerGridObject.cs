e383758fe24088cf75c3ccc73b635595b747a282
Tyler Bakke <tylerbakke@gmail.com>
2016-11-19 14:42:43 -0800

    Plants are placed at Farmer position

--- Assets/Scripts/GridObjects/PlayerGridObject.cs
+++ Assets/Scripts/GridObjects/PlayerGridObject.cs
@@ -132,25 +132,25 @@ public class PlayerGridObject : MoveableGridObject {
 			switch (direction) {
 				case Globals.Direction.East:
 					if (!eastHitCollider.isTriggered) {
-						PlantGridObject newPlant = (PlantGridObject)Instantiate (plants[plantNumber], new Vector3 (transform.position.x + 1, transform.position.y, 0), Quaternion.identity);
+						PlantGridObject newPlant = (PlantGridObject)Instantiate (plants[plantNumber], new Vector3 (transform.position.x, transform.position.y, 0), Quaternion.identity);
 						newPlant.Rotate(direction);
 					}
 					break;
 				case Globals.Direction.West:
 					if (!westHitCollider.isTriggered) {
-						PlantGridObject newPlant = (PlantGridObject)Instantiate (plants[plantNumber], new Vector3 (transform.position.x - 1, transform.position.y, 0), Quaternion.identity);
+						PlantGridObject newPlant = (PlantGridObject)Instantiate (plants[plantNumber], new Vector3 (transform.position.x, transform.position.y, 0), Quaternion.identity);
 						newPlant.Rotate(direction);
 					}
 					break;
 				case Globals.Direction.South:
 					if (!southHitCollider.isTriggered) {
-						PlantGridObject newPlant = (PlantGridObject)Instantiate (plants[plantNumber], new Vector3 (transform.position.x, transform.position.y - 1, 0), Quaternion.identity);
+						PlantGridObject newPlant = (PlantGridObject)Instantiate (plants[plantNumber], new Vector3 (transform.position.x, transform.position.y, 0), Quaternion.identity);
 						newPlant.Rotate(direction);
 					}
 					break;
 				case Globals.Direction.North:
 					if (!northHitCollider.isTriggered) {
-						PlantGridObject newPlant = (PlantGridObject)Instantiate (plants[plantNumber], new Vector3 (transform.position.x, transform.position.y + 1, 0), Quaternion.identity);
+						PlantGridObject newPlant = (PlantGridObject)Instantiate (plants[plantNumber], new Vector3 (transform.position.x, transform.position.y, 0), Quaternion.identity);
 						newPlant.Rotate(direction);
 					}
 					break;
