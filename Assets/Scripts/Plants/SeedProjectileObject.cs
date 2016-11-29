cbd06fb0532bf33676aaff2ed88edf85df29e845
andrewhongnguyen <andrewhongnguyen@gmail.com>
2016-11-28 19:05:23 -0800

    "watermelon plant is semi functional"

--- Assets/Scripts/Plants/SeedProjectileObject.cs
+++ Assets/Scripts/Plants/SeedProjectileObject.cs
@@ -1,30 +1,75 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SeedProjectileObject : MoveableGridObject {
+public class SeedProjectileObject : MonoBehaviour {
 
-    // Use this for initialization
+    public int shotSpeed;
+    public int shotRange;
+    private int shotRangeCounter;
+    public int damage;
+
     EnemyGridObject enemy;
 
-	void Start () {
-        base.Start();
-        //animator = GetComponent<Animator>();
-    }
-	
-	// Update is called once per frame
-	void Update () {
-
-        base.Update();
-        //enemyGridObject.TakeDamage(10);
-    }
-    void onTriggerEnter2D(Collider2D other)
+    // Use this for initialization
+    void Start()
     {
-        if(other.gameObject.tag == "Enemy")
+        shotRangeCounter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shotRangeCounter < shotRange)
         {
-            enemy = other.GetComponent<EnemyGridObject>();
-            Destroy(other.gameObject); //testing code
-            //enemy.TakeDamage(10); //real code
+            Mover(shotSpeed);
+            shotRangeCounter++;
+        }
+        else if(this.gameObject != null)
+        {
             Destroy(this.gameObject);
+        }
+    }
+    public void Mover(int shotSpeed)
+    {
+        Vector3 position = this.transform.position;
+        position.y += Globals.pixelSize * shotSpeed;
+        this.transform.position = position;
+        /**if (direction == Globals.Direction.South)
+        {
+            Vector3 position = this.transform.position;
+            position.y -= Globals.pixelSize;
+            this.transform.position = position;
+        }
+        else if (direction == Globals.Direction.West)
+        {
+            Vector3 position = this.transform.position;
+            position.x -= Globals.pixelSize;
+            this.transform.position = position;
+        }
+        else if (direction == Globals.Direction.North)
+        {
+            Vector3 position = this.transform.position;
+            position.y += Globals.pixelSize;
+            this.transform.position = position;
+        }
+        else if (direction == Globals.Direction.East)
+        {
+            Vector3 position = this.transform.position;
+            position.x += Globals.pixelSize;
+            this.transform.position = position;
+        }**/
+    }
+    void OnTriggerEnter(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(this.gameObject);
+            enemy = other.gameObject.GetComponent<EnemyGridObject>();
+            enemy.TakeDamage(damage);
+        }
+        else
+        {
+            Destroy(other.gameObject);
         }
     }
 }
