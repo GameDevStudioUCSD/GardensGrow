018464de06e815e3df4ab108e99b89278227db94
Steven Lee <ssl024@ucsd.edu>
2016-12-02 22:27:09 -0800

    Change unity in editor stuff for GridObject and TerrainObject
    GridObject no longer has ExecuteInEditMode and snap to grid.
    Use snap to grid script file so that later we can use Unity's snap settings.
    TerrainObject's randomizer changed so it no longer has ExecuteInEditMode.
    If you want to randomize sprite, right-click on the 'chosen sprite' field and click 'Randomize'

--- Assets/Scripts/GridObjects/GridObject.cs
+++ Assets/Scripts/GridObjects/GridObject.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 
-[ExecuteInEditMode]
 public class GridObject : MonoBehaviour {
 	public float snapValue = 1;
     public float depth = 0; 
@@ -9,19 +8,5 @@ public class GridObject : MonoBehaviour {
 	void Start () {
 	}
 
-	protected virtual void Update() {
-		if (!Application.isPlaying) {
-         float snapInverse = 1/snapValue;
-         
-         float x, y, z;
-         
-         // if snapValue = .5, x = 1.45 -> snapInverse = 2 -> x*2 => 2.90 -> round 2.90 => 3 -> 3/2 => 1.5
-         // so 1.45 to nearest .5 is 1.5
-         x = Mathf.Round(transform.position.x * snapInverse)/snapInverse;
-         y = Mathf.Round(transform.position.y * snapInverse)/snapInverse;   
-         z = depth;  // depth from camera
-         
-         transform.position = new Vector3(x, y, z);
-         }
-     }
+	protected virtual void Update() {}
 }
