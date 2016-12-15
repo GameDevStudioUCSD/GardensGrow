cd234e5c125ed89d41494dd54c405566048c3177
andrewhongnguyen <andrewhongnguyen@gmail.com>
2016-12-15 03:39:02 -0800

    Watermelon update

--- Assets/Scripts/Plants/WatermelonPlantObject.cs
+++ Assets/Scripts/Plants/WatermelonPlantObject.cs
@@ -34,16 +34,6 @@ public class WatermelonPlantObject : PlantGridObject
     // Update is called once per frame
     void Update()
     {
-
-        
-
-        if (counter > shotDelay)
-        {
-            Shooter();
-            counter = 0;
-        }
-        counter++;
-
         base.Update();
     }
 
@@ -51,28 +41,50 @@ public class WatermelonPlantObject : PlantGridObject
     {
 		seed.dir = direction;
 
-        Vector3 spawnPosition = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, 0.0f);
-        Quaternion spawnRotation = Quaternion.identity;
-        Instantiate(seed, spawnPosition, spawnRotation);
-
         if (direction == Globals.Direction.North)
         {
+            Vector3 spawnPosition = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, 0.0f);
+            Quaternion spawnRotation = Quaternion.Euler(0, 0, 270f);
+            Instantiate(seed, spawnPosition, spawnRotation);
+
             animator.SetInteger("Directions", 2);  
         }
         else if (direction == Globals.Direction.West)
         {
+            Vector3 spawnPosition = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, 0.0f);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(seed, spawnPosition, spawnRotation);
             animator.SetInteger("Directions", 0);
         }
         else if (direction == Globals.Direction.South)
         {
+            Vector3 spawnPosition = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, 0.0f);
+            Quaternion spawnRotation = Quaternion.Euler(0, 0, 90f);
+            Instantiate(seed, spawnPosition, spawnRotation);
             animator.SetInteger("Directions", 3);
         }
-        else
+        else if(direction == Globals.Direction.East)
         {
+            Vector3 spawnPosition = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, 0.0f);
+            Quaternion spawnRotation = Quaternion.Euler(0, 0, 180f);
+            Instantiate(seed, spawnPosition, spawnRotation);
             animator.SetInteger("Directions", 1);
         }
-    }
 
+        //animator.Stop();
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            if (counter > shotDelay)
+            {
+                Shooter();
+                counter = 0;
+            }
+            counter++;
+        }
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         
