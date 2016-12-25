398ffe645fb64a5b0c9548c29ecf82a310babb44
Eric Cheng <ericcheng95@gmail.com>
2016-12-24 18:28:18 -0800

    added portal for level switching

---
+++ Assets/Scripts/GridObjects/PortalObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalObject : StaticGridObject {
+	// 0 = Main Menu
+	// 1 = Overworld
+	// 2 = Lava Dungeon
+	// 3 = Lava Dungeon Boss
+
+	public int levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.CompareTag("Player"))
+        {
+        	Application.LoadLevel(levelToLoad);
+        }
+	}
+}
