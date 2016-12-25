b23e5fa527e1c82f63faacdf170e4b6ce22d7022
Eric Cheng <ericcheng95@gmail.com>
2016-12-25 14:36:00 -0800

    added some boss scripts

--- Assets/Scripts/EnemyAI/LavaBossAI.cs
+++ Assets/Scripts/EnemyAI/LavaBossAI.cs
@@ -2,14 +2,34 @@
 using System.Collections;
 
 public class LavaBossAI : KillableGridObject {
+	public EnemySpawner[] spawners;
+
+	private int currentSpawnerIndex;
+
 
 	// Use this for initialization
 	void Start () {
 		base.Start();
+		currentSpawnerIndex = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		base.Update();
+	}
+
+	// Makes the boss "take over" a spawner and move to the new location"
+	void Move () {
+		// Chooses a spawner index 0-3
+		int spawnerToJumpTo = (int)Random.Range(0,4);
+		spawners[currentSpawnerIndex].enabled = true;
+		currentSpawnerIndex = spawnerToJumpTo;
+
+		Vector3 newPosition = spawners[currentSpawnerIndex].gameObject.transform.position;
+
+		// 'Disables' the spawner so that the boss can take over it
+		spawners[currentSpawnerIndex].enabled = false;
+
+		this.transform.position = newPosition;
 	}
 }
