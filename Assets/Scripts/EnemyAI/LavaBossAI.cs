6a975eef5d546201beb966396fb3a3171d2ab0f0
Eric Cheng <ericcheng95@gmail.com>
2016-12-26 18:41:40 -0800

    added boss jumping

--- Assets/Scripts/EnemyAI/LavaBossAI.cs
+++ Assets/Scripts/EnemyAI/LavaBossAI.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class LavaBossAI : KillableGridObject {
-	public EnemySpawner[] spawners;
+	public GameObject[] spawners;
 	public Vector3[] boatLocations;
 	public GameObject boat;
 
@@ -21,11 +21,12 @@ public class LavaBossAI : KillableGridObject {
 	void Update () {
 		base.Update();
 		int numSpawns = 0;
-
+		EnemySpawner current;
 		if (state == BossState.Dormant) {
 			for (int i = 0; i < 4; i++) {
 				if (i != currentSpawnerIndex) {
-					numSpawns += spawners[i].numSpawns();
+					current = (EnemySpawner)spawners[i].GetComponent<EnemySpawner>();
+					numSpawns += current.numSpawns();
 				}
 			}
 			if (numSpawns == 0) {
@@ -33,8 +34,11 @@ public class LavaBossAI : KillableGridObject {
 			}
 		}
 		if (state == BossState.Emerging) {
+			int evenOrOdd = (int)Random.Range(0,2);
 			for (int i = 0; i < 4; i++) {
-				Instantiate(boat, boatLocations[i], Quaternion.identity);
+				if (i % 2 == evenOrOdd) {
+					Instantiate(boat, boatLocations[i], Quaternion.identity);
+				}
 			}
 			state = BossState.Emerged;
 		}
@@ -44,13 +48,17 @@ public class LavaBossAI : KillableGridObject {
 	void Move () {
 		// Chooses a spawner index 0-3
 		int spawnerToJumpTo = (int)Random.Range(0,4);
-		spawners[currentSpawnerIndex].enabled = true;
-		currentSpawnerIndex = spawnerToJumpTo;
-
-		Vector3 newPosition = spawners[currentSpawnerIndex].gameObject.transform.position;
 
 		// 'Disables' the spawner so that the boss can take over it
-		spawners[currentSpawnerIndex].enabled = false;
+		spawners[spawnerToJumpTo].SetActive(false);
+
+		// Moves the boss to the spawner's location
+		Vector3 newPosition = spawners[spawnerToJumpTo].gameObject.transform.position;
+
+		// 'Enables' the spawner that the boss was formerly at
+		spawners[currentSpawnerIndex].SetActive(true);
+
+		currentSpawnerIndex = spawnerToJumpTo;
 
 		this.transform.position = newPosition;
 	}
@@ -61,6 +69,7 @@ public class LavaBossAI : KillableGridObject {
     	{
         	//gameObject.GetComponent<Animation>().Play("Damaged");
         	state = BossState.Dormant;
+        	Move();
         	return base.TakeDamage(dmg);
         }
 
