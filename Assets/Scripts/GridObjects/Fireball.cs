0e2ce468f1ae03e67db4755df34081c8957a8bbe
Eric Cheng <ericcheng95@gmail.com>
2016-12-29 20:16:59 -0800

    updated meteors in boss

--- Assets/Scripts/GridObjects/Fireball.cs
+++ Assets/Scripts/GridObjects/Fireball.cs
@@ -3,20 +3,26 @@ using System.Collections;
 
 public class Fireball : StaticGridObject
 {
+	public int preFallTime = 100;
+
 	private float fallingSpeed;
 	private const int FALLING_SPEED_MAX = 30;
 	private const int FALLING_SPEED_MIN = 10;
 	private const float FALLING_SPEED_INCR = 200f;
 	private const float eps = FALLING_SPEED_MAX / FALLING_SPEED_INCR;
 
-	private enum FireballState { Falling, Fallen }
+	private enum FireballState { PreFalling, Falling, Fallen }
 	private FireballState state;
 
 	public GameObject ball;
 
+	private PlayerGridObject player;
+	private int fallFrames;
+
 	void Start() {
 		fallingSpeed = Random.Range(FALLING_SPEED_MIN, FALLING_SPEED_MAX) / FALLING_SPEED_INCR;
-		state = FireballState.Falling;
+		state = FireballState.PreFalling;
+		fallFrames = 0;
 
 		base.Start();
 	}
@@ -24,28 +30,45 @@ public class Fireball : StaticGridObject
 	// Update is called once per frame
 	void Update() {
 		base.Update();
+		if (state == FireballState.PreFalling) {
+			if (fallFrames >= preFallTime) {
+				state = FireballState.Falling;
+			}
+			else {
+				fallFrames++;
+			}
+		}
+		if (state == FireballState.Fallen) {
+			if (player)
+				player.TakeDamage(5);
+			Destroy(gameObject);
 
-		if (state == FireballState.Fallen) return;
+			return;
+		}
 
-		if (Mathf.Abs(ball.transform.position.y - transform.position.y) > eps) {
-			Vector3 newPosition = ball.transform.position;
-			newPosition.y -= fallingSpeed;
-			ball.transform.position = newPosition;
-		} else {
-			ball.transform.position = transform.position;
+		if (state == FireballState.Falling) {
+			ball.SetActive(true);
+			if (Mathf.Abs(ball.transform.position.y - transform.position.y) > eps) {
+				Vector3 newPosition = ball.transform.position;
+				newPosition.y -= fallingSpeed;
+				ball.transform.position = newPosition;
+			} else {
+				ball.transform.position = transform.position;
 
-			state = FireballState.Fallen;
+				state = FireballState.Fallen;
+			}
 		}
 	}
 
 	void OnTriggerEnter2D(Collider2D col) {
-		if (state != FireballState.Fallen) return;
-
 		if (col.gameObject.CompareTag("Player")) {
-			PlayerGridObject player = col.GetComponentInParent<PlayerGridObject>();
-			player.TakeDamage(5);
+			player = col.GetComponentInParent<PlayerGridObject>();
+		}
+	}
 
-			Destroy(gameObject);
+	void OnTriggerExit2D(Collider2D col) {
+		if (col.gameObject.CompareTag("Player")) {
+			player = null;
 		}
 	}
 }
