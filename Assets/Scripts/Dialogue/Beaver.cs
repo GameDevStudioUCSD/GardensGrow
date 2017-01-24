12d56eff2cda2dcd2b54a87e8334cd7e942da353
andrewhongnguyen <andrewhongnguyen@gmail.com>
2017-01-24 12:37:30 -0800

    minor changes

---
+++ Assets/Scripts/Dialogue/Beaver.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Beaver : MonoBehaviour
+{
+    public Globals.Direction direction;
+   
+    void Update()
+    {
+        if (direction == Globals.Direction.East)
+        {
+            this.gameObject.transform.Rotate(new Vector3(0, 0, 0));
+        }
+        else if (direction == Globals.Direction.West)
+        {
+
+            this.gameObject.transform.Rotate(new Vector3(0, 180, 0));
+        }
+    }
+
+}
