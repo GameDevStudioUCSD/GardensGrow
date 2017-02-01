4e92df613a3150ce6937a298820ded674ac045f9
Eric Cheng <ericcheng95@gmail.com>
2017-01-31 16:07:56 -0800

    added some boss stuff

---
+++ Assets/Scripts/EnemyAI/WindBossAI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindBossAI : KillableGridObject {
+	public enum BossState { SpawningRocks, SpawningMonsters, Idle, Inhaling, Blowing };
+	private BossState state;
+
+	// Use this for initialization
+	protected override void Start () {
+		base.Start();
+		state = BossState.SpawningRocks;
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+		base.Update();
+
+		if (state == BossState.SpawningRocks) {
+			SpawnRocks();	
+			state = BossState.SpawningMonsters;
+		}
+	}
+
+	void SpawnRocks() {
+		Debug.Log("spawn rocks");
+	}
+}
