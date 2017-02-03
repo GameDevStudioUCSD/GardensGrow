4cc6926e0e180d171384fbd5841f8ec6e3af8042
Daniel <griffithsdanielc@gmail.com>
2017-02-02 20:44:45 -0800

    Timed Switches

--- Assets/Scripts/TriggerObjects/Switch.cs
+++ Assets/Scripts/TriggerObjects/Switch.cs
@@ -6,26 +6,40 @@ public class Switch : KillableGridObject {
 
     public UnityEvent pressEvent;
     public UnityEvent unpressEvent;
+    public bool timed = false;
+    public int timeout = 0;
+    private int timer = 0;
     private bool pressed;
     private Animator animator;
 
     // Use this for initialization
-    protected override void Start()
-    {
+    protected override void Start() {
         base.Start();
         pressed = false;
         //animator = this.gameObject.GetComponent<Animator>();
     }
 
-    public override bool TakeDamage(int damage)
-    {
-        if (!pressed)
-        {
+    protected override void Update() {
+        base.Update();
+        if (pressed && timed) {
+            timer--;
+            if (timer <= 0) {
+                unpressEvent.Invoke();
+                pressed = false;
+            }
+        }
+    }
+
+    public override bool TakeDamage(int damage) {
+        if (!pressed) {
             pressEvent.Invoke();
             pressed = true;
+            if (timed) timer = timeout;
         }
-        else
-        {
+        else if (timed) {
+            timer = timeout;
+        }
+        else {
             unpressEvent.Invoke();
             pressed = false;
         }
