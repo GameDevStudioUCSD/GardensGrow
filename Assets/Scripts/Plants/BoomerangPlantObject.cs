353ae7af54950aec7bd75fe5db029771f5d4a27f
Sung Rim Huh <sungrimhuh@gmail.com>
2017-02-11 16:59:48 -0800

    Add Boomerang Plant (except images)

---
+++ Assets/Scripts/Plants/BoomerangPlantObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoomerangPlantObject : PlantGridObject {
+
+    [SerializeField]
+    private BoomerangPlantProjectileObject boomerangScript;
+
+    void Start () {
+    }
+
+    void Update () {
+    }
+
+    private void OnBecameVisible() {
+        boomerangScript.AddPlant(transform.position);
+    }
+
+    private void OnBecameInvisible() {
+        boomerangScript.RemovePlant(transform.position);
+    }
+}
