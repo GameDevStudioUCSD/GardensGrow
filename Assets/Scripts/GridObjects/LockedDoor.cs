f3b65e9e71ab1255312383d61bc11cb30a51b7db
Eric Cheng <ericcheng95@gmail.com>
2017-02-21 21:08:23 -0800

    created door prefab

---
+++ Assets/Scripts/GridObjects/LockedDoor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockedDoor : MonoBehaviour {
+	public GameObject barrier;
+	public bool unlockable;
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = this.gameObject.GetComponent<Animator>();
+	}
+
+	void UnlockDoor() {
+		animator.SetBool("Opening", true);
+		barrier.gameObject.SetActive(false);
+	}
+
+	void OnTriggerEnter2d(Collider other) {
+		if (unlockable) {
+			if (other.gameObject.tag == "Player") {
+				if (Globals.numKeys > 0) {
+					Globals.numKeys--;
+					PlayerGridObject player = other.GetComponent<PlayerGridObject>();
+					UIController controller = player.canvas;
+
+					controller.UpdateUI();
+
+					UnlockDoor();
+				}
+			}
+		}
+	}
+}
