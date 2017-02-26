91f40146f7f675f7133f0826ec620aa9dfb913fb
Sung Rim Huh <sungrimhuh@gmail.com>
2017-02-25 16:31:52 -0800

    Add light plant (mushroom)

---
+++ Assets/Scripts/Plants/LightPlantObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightPlantObject : PlantGridObject {
+
+    [SerializeField]
+    private int radius;
+
+    [SerializeField]
+    private int lightLevel;
+
+    void OnEnable() {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, radius);
+        foreach (Collider2D collider in colliders) {
+            LightLevel ll = collider.GetComponent<LightLevel>();
+            if (ll != null) {
+                ll.Brighten(lightLevel);
+            }
+        }
+    }
+
+    void OnDisable() {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, radius);
+        foreach (Collider2D collider in colliders) {
+            LightLevel ll = collider.GetComponent<LightLevel>();
+            if (ll != null) {
+                ll.Dim(lightLevel);
+            }
+        }
+    }
+}
