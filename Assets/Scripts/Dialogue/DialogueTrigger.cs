8b2cf24de24ac11aa354c698adc70c4432cdb896
andrewhongnguyen <andrewhongnguyen@gmail.com>
2017-03-03 17:37:31 -0800

    npc/dialogue/text system MAJOR change

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -5,6 +5,7 @@ public class DialogueTrigger : MonoBehaviour {
 	private UIController canvas;
 	public string textFileName;
 	public Collider2D activeRegionTrigger;
+    private bool isTalkingToPlayer = false;
 	public PlayerGridObject player;
 
 	private GameObject dialogue;
@@ -17,17 +18,35 @@ public class DialogueTrigger : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (activeRegionTrigger.bounds.Contains(player.transform.position))
+        {
+            if (!isTalkingToPlayer)
+            {
+                isTalkingToPlayer = true;
+                canvas.ShowDialog();
+                dialogue.GetComponentInChildren<DialogueSystem>().textFile = Resources.Load<TextAsset>("Text/" + textFileName);
+                dialogue.GetComponentInChildren<DialogueSystem>().LoadText();
+            }
+        }
+        else if (!activeRegionTrigger.bounds.Contains(player.transform.position) && isTalkingToPlayer)
+        {
+            canvas.EndDialog();
+            isTalkingToPlayer = false;
+        }
+        if (!dialogue.activeSelf && activeRegionTrigger.bounds.Contains(player.transform.position) &&
+             (Input.GetKeyDown(KeyCode.Tab) || Input.GetKeyDown(KeyCode.Return)) && isTalkingToPlayer)
+        {
+            canvas.ShowDialog();
+            dialogue.GetComponentInChildren<DialogueSystem>().textFile = Resources.Load<TextAsset>("Text/" + textFileName);
+            dialogue.GetComponentInChildren<DialogueSystem>().LoadText();
+        }
+        /*
 		if (!dialogue.activeSelf && activeRegionTrigger.bounds.Contains(player.transform.position) && 
 			(Input.GetKeyDown(KeyCode.Tab) || Input.GetKeyDown(KeyCode.Return))) {
-
-            //NOTE: this code SHOULD work to pause the game if talking to sign 
-            //Time.timeScale = 0;
-            //canvas.paused = true;
             canvas.ShowDialog();
 
 			dialogue.GetComponentInChildren<DialogueSystem> ().textFile = Resources.Load<TextAsset>("Text/" + textFileName);
 			dialogue.GetComponentInChildren<DialogueSystem> ().LoadText ();
-            //canvas.paused = false;
-		}
-	}
+		}*/
+    }
 }
