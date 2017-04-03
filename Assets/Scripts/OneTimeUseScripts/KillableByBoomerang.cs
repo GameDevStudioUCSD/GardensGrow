6449a5e81202743a1f735324fd803aa3bd42a6e0
andrewhongnguyen <andrewhongnguyen@gmail.com>
2017-04-03 12:46:20 -0700

    minor changes

---
+++ Assets/Scripts/OneTimeUseScripts/KillableByBoomerang.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillableByBoomerang : MonoBehaviour {
+
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<Boomerang>() && this != null)
+        {
+            Debug.Log("have the boomerang 3");
+            Destroy(this.gameObject);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<Boomerang>())
+        {
+            Debug.Log("have the boomerang 1");
+            Destroy(this.gameObject);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.GetComponent<Boomerang>())
+        {
+            Debug.Log("have the boomerang 2");
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/OneTimeUseScripts/KillableTreePartner.cs
+++ Assets/Scripts/OneTimeUseScripts/KillableTreePartner.cs
@@ -4,5 +4,4 @@ using System.Collections;
 public class KillableTreePartner : MonoBehaviour {
 
     public KillableTree[] list = new KillableTree[3];
-	// Use this for initialization
 }
