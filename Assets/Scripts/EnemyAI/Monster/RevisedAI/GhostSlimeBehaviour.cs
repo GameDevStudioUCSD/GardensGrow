b47f9390d61c290310557e5704d15611ebbb72a0
Daniel <griffithsdanielc@gmail.com>
2017-04-04 20:44:54 -0700

    Ghost Slime animator

---
+++ Assets/Scripts/EnemyAI/Monster/RevisedAI/GhostSlimeBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostSlimeBehaviour : GenericMonsterBehaviour {
+
+    int lightTimer = 0;
+
+    protected override void Start() {
+        base.Start();
+        animator.SetTrigger("Darken");
+        isInvulnerable = true;
+    }
+
+	protected override void Update() {
+        base.Update();
+        lightTimer++;
+        if (lightTimer >= 200) {
+            lightTimer = 0;
+            if (isInvulnerable) Lighten();
+            else Darken();
+        }
+	}
+
+    public override void Move(Globals.Direction direction) { }
+
+    public void Lighten() {
+        animator.SetTrigger("Lighten");
+        isInvulnerable = false;
+    }
+
+    public void Darken() {
+        animator.SetTrigger("Darken");
+        isInvulnerable = true;
+    }
+}
