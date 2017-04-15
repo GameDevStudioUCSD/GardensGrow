0e9e6db12d713d0b63e2374e8364d7b328129814
Sung Rim Huh <sungrimhuh@gmail.com>
2017-04-15 15:59:43 -0700

    Separate light mechanic from plant

--- Assets/Scripts/Plants/LightPlantObject.cs
+++ Assets/Scripts/Plants/LightPlantObject.cs
@@ -2,30 +2,4 @@
 using System.Collections;
 
 public class LightPlantObject : PlantGridObject {
-
-    public int radius;
-
-    public float lightLevel;
-
-    void OnEnable() {
-        ChangeAdjacentLightLevel(1.0f / lightLevel);
-    }
-
-    void OnDisable() {
-        ChangeAdjacentLightLevel(-1.0f / lightLevel);
-    }
-
-    void ChangeAdjacentLightLevel(float amount) {
-        Vector2 tilePos = new Vector2(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
-        for (int i = 1; i <= radius; i += 1) {
-            Vector2 offset = new Vector2(i, i);
-            Collider2D[] colliders = Physics2D.OverlapAreaAll(tilePos - offset, tilePos + offset);
-            foreach (Collider2D collider in colliders) {
-                LightLevel ll = collider.GetComponent<LightLevel>();
-                if (ll != null) {
-                    ll.ChangeLightLevel(amount);
-                }
-            }
-        }
-    }
 }
