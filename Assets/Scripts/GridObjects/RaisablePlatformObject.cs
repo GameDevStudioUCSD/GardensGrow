e034f83401625826ad13a4a5f4b79f9f8008ca96
Eric Cheng <ericcheng95@gmail.com>
2017-04-22 15:19:12 -0700

    raisable platform for dungeon is done, but no sprites

---
+++ Assets/Scripts/GridObjects/RaisablePlatformObject.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaisablePlatformObject : MonoBehaviour {
+
+	bool raised = false;
+	public GameObject bossEntrance;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (raised == true) {
+			if (col.gameObject.CompareTag("Player")) {
+	            PlayerGridObject player = col.GetComponent<PlayerGridObject>();
+	            player.onPlatform = true;
+	        }
+        }
+    }
+
+	void OnTriggerExit2D(Collider2D col) {
+		if (raised == true) {
+			if (col.gameObject.CompareTag("Player")) {
+	            PlayerGridObject player = col.GetComponent<PlayerGridObject>();
+	            player.onPlatform = false;
+	        }
+        }
+    }
+
+    public void Toggle() {
+    	if (raised == false) {
+    		raised = true;
+    		bossEntrance.SetActive(true);
+    	}
+    	else {
+    		raised = false;
+    		bossEntrance.SetActive(false);
+    	}
+    }
+}
