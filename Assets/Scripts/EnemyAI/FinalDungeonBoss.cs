0329c82813ec2b87a5952d377e165765faadd4c7
andrewhongnguyen <andrewhongnguyen@gmail.com>
2017-05-06 17:03:56 -0700

    is working on final final boss

---
+++ Assets/Scripts/EnemyAI/FinalDungeonBoss.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinalDungeonBoss : MonoBehaviour {
+
+    //spawning information
+    public GameObject tentacle;
+    private Quaternion spawnRotationHorizontal = Quaternion.Euler(0, 0, 90.0f);
+    private Quaternion spawnRotationVertical = Quaternion.Euler(0, 0, 0f);
+
+    //spawning locations
+    private Vector3 spawnPositionNorth = new Vector3(0.0f, 10.0f, 0.0f);
+    private Vector3 spawnPositionSouth = new Vector3(0.0f, -8.0f, 0.0f);
+    private Vector3 spawnPositionEast = new Vector3(8.0f, -2.0f, 0.0f);
+    private Vector3 spawnPositionWest = new Vector3(-15.0f, -2.0f, 0.0f);
+
+    //booleans to control boss state
+    private GameObject currentTentacle;
+    private int currSituation;
+    private bool spawnedActiveTentacle = false;
+    private int counter = 0;
+
+	// Update is called once per frame
+	void Update () {
+
+        if (spawnedActiveTentacle)
+        {
+            counter++;
+            if (counter > 80)
+            {
+                if(counter > 250)
+                {
+                    Destroy(currentTentacle.gameObject);
+                    spawnedActiveTentacle = false;
+                    counter = 0;
+                }
+                if (currSituation == 0)
+                {
+                    currentTentacle.GetComponent<Tentacle>().Move(Globals.Direction.South);
+                }
+                else if (currSituation == 1)
+                {
+                    currentTentacle.GetComponent<Tentacle>().Move(Globals.Direction.North);
+                }
+                else if (currSituation == 2)
+                {
+                    currentTentacle.GetComponent<Tentacle>().Move(Globals.Direction.East);
+                }
+                else if (currSituation == 3)
+                {
+                    currentTentacle.GetComponent<Tentacle>().Move(Globals.Direction.West);
+                }
+            }
+            else
+            {
+                if (currSituation == 0)
+                {
+                    currentTentacle.GetComponent<Tentacle>().Move(Globals.Direction.North);
+                }
+                else if (currSituation == 1)
+                {
+                    currentTentacle.GetComponent<Tentacle>().Move(Globals.Direction.South);
+                }
+                else if (currSituation == 2)
+                {
+                    currentTentacle.GetComponent<Tentacle>().Move(Globals.Direction.West);
+                }
+                else if (currSituation == 3)
+                {
+                    currentTentacle.GetComponent<Tentacle>().Move(Globals.Direction.East);
+                }
+            }
+        }
+        else
+        {
+            currSituation = Random.Range(0, 3);
+            if (currSituation == 0)
+            {
+                currentTentacle = (GameObject)Instantiate(tentacle, spawnPositionSouth, spawnRotationVertical);
+            }
+            else if(currSituation == 1)
+            {
+                currentTentacle = (GameObject)Instantiate(tentacle, spawnPositionNorth, spawnRotationVertical);
+            }
+            else if(currSituation == 2)
+            {
+                currentTentacle = (GameObject)Instantiate(tentacle, spawnPositionEast, spawnRotationHorizontal);
+            }
+            else if(currSituation == 3)
+            {
+                currentTentacle = (GameObject)Instantiate(tentacle, spawnPositionWest, spawnRotationHorizontal);
+            }
+            currentTentacle.GetComponent<Tentacle>().speed = 3.0f;
+            spawnedActiveTentacle = true;
+
+        }
+	}
+}
---
+++ Assets/Scripts/EnemyAI/Tentacle.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tentacle : MonoBehaviour {
+
+    public float speed = 1.0f;
+    public int damage = 1;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<PlayerGridObject>())
+        {
+            other.gameObject.GetComponent<PlayerGridObject>().TakeDamage(1);
+        }
+    }
+    public void Move(Globals.Direction direction)
+    {
+        if (direction == Globals.Direction.South)
+        {
+            Vector3 position = this.transform.position;
+            position.y -= Globals.pixelSize*speed;
+            this.transform.position = position;
+        }
+        else if (direction == Globals.Direction.West)
+        {
+            Vector3 position = this.transform.position;
+            position.x -= Globals.pixelSize*speed;
+            this.transform.position = position;
+        }
+        else if (direction == Globals.Direction.North)
+        {
+            Vector3 position = this.transform.position;
+            position.y += Globals.pixelSize * speed;
+            this.transform.position = position;
+        }
+        else if (direction == Globals.Direction.East)
+        {
+            Vector3 position = this.transform.position;
+            position.x += Globals.pixelSize * speed;
+            this.transform.position = position;
+        }
+    }
+}
