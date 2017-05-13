0fa55757e6e644e21bbcd888efec8acb967d0db7
Daniel <griffithsdanielc@gmail.com>
2017-05-12 19:14:08 -0700

    Fixing NPC pushing bug, fixing NPC dialogue freeze if player attacks

--- Assets/Scripts/GridObjects/PlayerGridObject.cs
+++ Assets/Scripts/GridObjects/PlayerGridObject.cs
@@ -113,7 +113,7 @@ public class PlayerGridObject : MoveableGridObject {
                 foreach (KillableGridObject target in killList)
                 {
                     MoveableGridObject moveable = target.GetComponent<MoveableGridObject>();
-                    if (moveable) {
+                    if (moveable && target.faction == Globals.Faction.Enemy) {
                         if (!(moveable.gameObject.GetComponent<BombObject>()) && !(moveable.gameObject.GetComponent<RollingBoulder>())) {
                             for (int i = 0; i < this.gameObject.GetComponent<PlayerGridObject>().knockBackPower; i++) {
                                 moveable.Move(this.gameObject.GetComponent<PlayerGridObject>().direction);
