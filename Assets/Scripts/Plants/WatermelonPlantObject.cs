cb433b7a6a7f5d98d01c1c6ace6a394970ff6e84
Daniel <griffithsdanielc@gmail.com>
2017-05-26 20:20:20 -0700

    Watermelon plant stops shooting when target dies

--- Assets/Scripts/Plants/WatermelonPlantObject.cs
+++ Assets/Scripts/Plants/WatermelonPlantObject.cs
@@ -9,7 +9,7 @@ public class WatermelonPlantObject : PlantGridObject
     public GameObject bullet;
     private int counter;
     public int shotDelay;
-    private bool triggered = false;
+    private KillableGridObject triggered;
     private UIController uic;
 
     public int changeDirectionWaitTime;
@@ -113,7 +113,7 @@ public class WatermelonPlantObject : PlantGridObject
 
             KillableGridObject killable = other.GetComponent<KillableGridObject>();
             if (!killable.isInvulnerable) {
-                triggered = true;
+                triggered = killable;
                 if (canChangeDir) {
                     if (other.IsTouching(southCollider.gameObject.GetComponent<BoxCollider2D>())) {
                         direction = Globals.Direction.South;
@@ -142,6 +142,6 @@ public class WatermelonPlantObject : PlantGridObject
 
     void OnTriggerExit2D(Collider2D other) {
         if (other.CompareTag("Enemy") || other.CompareTag("EnemySpawner"))
-            triggered = false;
+            triggered = null;
     }
 }
