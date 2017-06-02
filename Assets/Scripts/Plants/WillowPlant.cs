949ab050bcb69dd2698cfc3aeef072af96f8d41b
Eric Cheng <ericcheng95@gmail.com>
2017-06-02 15:03:10 -0700

    created willow plant script

---
+++ Assets/Scripts/Plants/WillowPlant.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WillowPlant : PlantGridObject {
+
+	private int counter;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		counter++;
+		if (counter >= 200) {
+			Debug.Log("Counter reached");
+			Application.LoadLevel(0);
+		}
+	}
+}
